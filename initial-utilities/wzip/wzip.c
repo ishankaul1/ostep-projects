f920e708629ec2868f63edcdf4a5c468475e9f75
Ishan Kaul <ishankaul2000@gmail.com>
2024-11-27 19:59:34 -0800

    impl of wzip that zips separately across files

---
+++ initial-utilities/wzip/wzip.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* read_file(FILE *fp, size_t* size_out) {
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    *size_out = size;
+    fseek(fp, 0, SEEK_SET);
+    char* buffer = malloc(size);
+
+    if (!buffer) return NULL;
+    
+    size_t read = fread(buffer, 1, size, fp);
+    if (read != size) {
+        free(buffer);
+        return NULL;
+    }
+    
+    return buffer;
+}
+
+
+int process_file(FILE *fp) {
+    if (fp == NULL)
+    {
+        printf("wzip: cannot open file\n");
+        exit(1);
+    }
+
+    size_t size_out;
+    // gets malloced in read_file
+    char* fileBuffer = read_file(fp, &size_out);
+
+    if (fileBuffer == NULL) {
+        printf("wzip: cannot open file\n");
+        exit(1);
+    }
+
+    int currentCount = 1;
+    char prev = '\0';
+    char cur;
+
+    for (int i = 0; i < size_out; i++){
+        cur = fileBuffer[i];
+        if (prev == '\0') {
+            prev = cur;
+        } else if (cur != prev) {
+            fwrite(&currentCount, sizeof(int), 1, stdout);
+            fwrite(&prev, sizeof(char), 1, stdout );
+            prev = cur;
+            currentCount = 1;
+        } else {
+            currentCount++;
+        }
+    }
+
+    fwrite(&currentCount, sizeof(int), 1, stdout);
+    fwrite(&prev, sizeof(char), 1, stdout );
+
+
+    free(fileBuffer);
+    return 1;
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        printf("wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    FILE* fp;
+
+    for (int i = 1; i < argc; i++) {
+        fp = fopen(argv[i], "r");
+        process_file(fp);
+        if (fp != stdin) {
+            fclose(fp);
+        }
+    }
+
+}
