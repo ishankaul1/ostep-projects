12418e9c8fc28c76dadeb6e396163df0cb16086d
Ishan Kaul <ishankaul2000@gmail.com>
2024-11-27 18:32:43 -0800

    wgrep generic impl with getline

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -2,39 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-// helper function: search for term in file
-
-
-int main(int argc, char* argv[]) {
-    if (argc == 1) {
-        printf("wgrep: searchterm [file ...]\n");
-        return 1;
+int process_file(FILE *fp, char *searchTerm)
+{
+    if (fp == NULL)
+    {
+        printf("wgrep: cannot open file\n");
+        exit(1);
     }
 
-    char* searchTerm = argv[1];
-    FILE *fp;
+    size_t len = 0;
+    ssize_t read;
 
-    if (argc == 2) {
-        fp = stdin;
-    }
-    for (int i = 2; i < argc; i++) {
-        fp = fopen(argv[i], "r");
-        if (fp == NULL) {
-            printf("wgrep: cannot open file\n");
-            exit(1);
-        }
-
-    }
-    // assume lines won't be longer than 10k chars
-    char lineBuffer[10000];
-    while (fgets(lineBuffer, sizeof(lineBuffer), fp) != NULL) {
-        if (strstr(lineBuffer, searchTerm) != NULL) {
+    char *lineBuffer = NULL;
+    while ((read = getline(&lineBuffer, &len, fp)) != -1)
+    {
+        if (strstr(lineBuffer, searchTerm) != NULL)
+        {
             printf("%s", lineBuffer);
         }
     }
 
-    if (fp != stdin) {
+    free(lineBuffer);
+    if (fp != stdin)
+    {
         fclose(fp);
+    }
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("wgrep: searchterm [file ...]\n");
+        return 1;
+    }
+
+    char *searchTerm = argv[1];
+    FILE *fp;
+
+    if (argc == 2)
+    {
+        fp = stdin;
+        process_file(fp, searchTerm);
+    }
+    else
+    {
+        for (int i = 2; i < argc; i++)
+        {
+            fp = fopen(argv[i], "r");
+            process_file(fp, searchTerm);
+        }
     }
 
     return 0;
